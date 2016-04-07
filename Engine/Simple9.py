80e3f260988d0b34b56d0736b54060bd8cfd0ea1
Pashna <p02p@yandex.ru>
2016-04-07 03:22:40 +0300

    Simple9 worksgit add .!

--- Engine/Simple9.py
+++ Engine/Simple9.py
@@ -1,10 +1,11 @@
+# coding: utf-8
 from struct import pack, unpack
 
 class Simple9():
 
     max_values = [1, 3, 7, 15, 31, 127, 511, 16383, 268435455]
-    max_length = [28,14,9,7,5,4,3, 2, 1]
-    shift = [1,2,3,4,5,7,9,14,28]
+    max_length = [28, 14, 9, 7, 5, 4, 3, 2, 1]
+    shift = [1, 2, 3, 4, 5, 7, 9, 14, 28]
     value_mask = (1<<28)-1
 
     """
@@ -13,13 +14,20 @@ class Simple9():
     """
 
     def __init__(self):
-        pass
+        self._decoders = [self._decode_number_0, self._decode_number_1, self._decode_number_2, self._decode_number_3, self._decode_number_4, self._decode_number_5, self._decode_number_6, self._decode_number_7, self._decode_number_8]
 
     def encode(self, numbers):
         i = 0
         encoded = ""
         while(i < len(numbers)):
             scheme, count_of_numbers = self._choice_type(numbers[i:])
+            if scheme == -1:
+                # Остаются пробелы, нельзя разместить полностью - нужно вносить избыточность
+                for j in range(i, len(numbers)):
+                    encoded += self.encode_numbers(numbers[j:j+1], 8)
+
+                break
+
             print scheme, count_of_numbers
             encoded += self.encode_numbers(numbers[i:i+count_of_numbers], scheme)
             i += count_of_numbers
@@ -41,7 +49,8 @@ class Simple9():
         numbers = unpack('%dI' % (len(bytestream)/4), bytestream)
         decoded = []
         for n in numbers:
-            decoded.extend(self.decode_number(n))
+            scheme = n >> 28
+            decoded.extend(self._decoders[scheme](n))
 
         return decoded
 
@@ -60,32 +69,34 @@ class Simple9():
 
         return result
 
-    def decode_number_0(self, number):
-        return [(number&134217728)>>54, (number&67108864)>>52, (number&33554432)>>50, (number&16777216)>>48, (number&8388608)>>46, (number&4194304)>>44, (number&2097152)>>42, (number&1048576)>>40, (number&524288)>>38, (number&262144)>>36, (number&131072)>>34, (number&65536)>>32, (number&32768)>>30, (number&16384)>>28, (number&8192)>>26, (number&4096)>>24, (number&2048)>>22, (number&1024)>>20, (number&512)>>18, (number&256)>>16, (number&128)>>14, (number&64)>>12, (number&32)>>10, (number&16)>>8, (number&8)>>6, (number&4)>>4, (number&2)>>2, (number&1)>>0, ]
 
-    def decode_number_1(self, number):
+    def _decode_number_0(self, number):
+        return [(number&134217728)>>27, (number&67108864)>>26, (number&33554432)>>25, (number&16777216)>>24, (number&8388608)>>23, (number&4194304)>>22, (number&2097152)>>21, (number&1048576)>>20, (number&524288)>>19, (number&262144)>>18, (number&131072)>>17, (number&65536)>>16, (number&32768)>>15, (number&16384)>>14, (number&8192)>>13, (number&4096)>>12, (number&2048)>>11, (number&1024)>>10, (number&512)>>9, (number&256)>>8, (number&128)>>7, (number&64)>>6, (number&32)>>5, (number&16)>>4, (number&8)>>3, (number&4)>>2, (number&2)>>1, (number&1)>>0, ]
+
+    def _decode_number_1(self, number):
         return [(number&201326592)>>26, (number&50331648)>>24, (number&12582912)>>22, (number&3145728)>>20, (number&786432)>>18, (number&196608)>>16, (number&49152)>>14, (number&12288)>>12, (number&3072)>>10, (number&768)>>8, (number&192)>>6, (number&48)>>4, (number&12)>>2, (number&3)>>0, ]
 
-    def decode_number_2(self, number):
-        return [(number&117440512)>>16, (number&14680064)>>14, (number&1835008)>>12, (number&229376)>>10, (number&28672)>>8, (number&3584)>>6, (number&448)>>4, (number&56)>>2, (number&7)>>0, ]
+    def _decode_number_2(self, number):
+        return [(number&117440512)>>24, (number&14680064)>>21, (number&1835008)>>18, (number&229376)>>15, (number&28672)>>12, (number&3584)>>9, (number&448)>>6, (number&56)>>3, (number&7)>>0, ]
 
-    def decode_number_3(self, number):
-        return [(number&251658240)>>12, (number&15728640)>>10, (number&983040)>>8, (number&61440)>>6, (number&3840)>>4, (number&240)>>2, (number&15)>>0, ]
+    def _decode_number_3(self, number):
+        return [(number&251658240)>>24, (number&15728640)>>20, (number&983040)>>16, (number&61440)>>12, (number&3840)>>8, (number&240)>>4, (number&15)>>0, ]
 
-    def decode_number_4(self, number):
-        return [(number&32505856)>>8, (number&1015808)>>6, (number&31744)>>4, (number&992)>>2, (number&31)>>0, ]
+    def _decode_number_4(self, number):
+        return [(number&32505856)>>20, (number&1015808)>>15, (number&31744)>>10, (number&992)>>5, (number&31)>>0, ]
 
-    def decode_number_5(self, number):
-        return [(number&266338304)>>6, (number&2080768)>>4, (number&16256)>>2, (number&127)>>0, ]
+    def _decode_number_5(self, number):
+        return [(number&266338304)>>21, (number&2080768)>>14, (number&16256)>>7, (number&127)>>0, ]
 
-    def decode_number_6(self, number):
-        return [(number&133955584)>>4, (number&261632)>>2, (number&511)>>0, ]
+    def _decode_number_6(self, number):
+        return [(number&133955584)>>18, (number&261632)>>9, (number&511)>>0, ]
 
-    def decode_number_7(self, number):
-        return [(number&268419072)>>2, (number&16383)>>0, ]
+    def _decode_number_7(self, number):
+        return [(number&268419072)>>14, (number&16383)>>0, ]
 
-    def decode_number_8(self, number):
+    def _decode_number_8(self, number):
         return [(number&268435455)>>0, ]
+
 
     def _choice_type(self, n_lst):
         current_scheme = 0
@@ -93,9 +104,11 @@ class Simple9():
         max_length = 28
 
         while(i < max_length):
-            if len(n_lst) == i:
-                pass
-            number = n_lst[i]
+            if i < len(n_lst):
+                number = n_lst[i]
+            else:
+                return -1, -1
+
             result_index = next((Simple9.max_values.index(n) for n in Simple9.max_values if n >= number), len(Simple9.max_values))
             if result_index > current_scheme:
                 current_scheme = result_index
@@ -107,26 +120,20 @@ class Simple9():
         return current_scheme, max_length
 
 
-s = Simple9()
-#print s.decode_number_0(262974714)
-#print s._choice_type([1, 3, 142, 24, 412])
-#ec = s.encode([1000, 2000, 25000, 30000])
-#print s.decode(ec)
-max_values = [1, 3, 7, 15, 31, 127, 511, 16383, 268435455]
-max_length = [28,14,9,7,5,4,3, 2, 1]
-shift = [1,2,3,4,5,7,9,14,28]
-
 def simple9_script_generator():
     for j in range(9):
         max_value = Simple9.max_values[j]
         shift = Simple9.shift[j]
         max_length = Simple9.max_length[j]
-        script = "def decode_number_{}(self, number):\n".format(j)
+        script = "def _decode_number_{}(self, number):\n".format(j)
         script += "    return ["
         for i in reversed(range(max_length)):
-            script += "(number&{})>>{}, ".format(max_value << shift*i, 2*i)
+            script += "(number&{})>>{}, ".format(max_value << shift*i, shift*i)
         script += "]\n"
         print script
 
-
-simple9_script_generator()
+if __name__ == "__main__":
+    s = Simple9()
+    enc = s.encode([5523123, 123, 12, 32])
+    print s.decode(enc)
+    simple9_script_generator()
